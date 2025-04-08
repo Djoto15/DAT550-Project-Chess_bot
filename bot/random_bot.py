a7a7c51907866e43deca6d8884e02ef375cfddde
Djoto15 <raph.gauthier02@gmail.com>
2025-04-08 12:29:57 +0200

    implementation of random bot play (not finished)

---
+++ bot/random_bot.py
@@ -0,0 +1,25 @@
+import random
+import chess
+
+class RandomBot():
+    def __init__(self, engine):
+        self.moves = None
+        self.engine = engine
+
+    def get_moves(self):
+        """
+        Get the legal moves that the bot can do.
+        """
+        legal_moves = self.engine.board.legal_moves
+        black_moves = [move for move in legal_moves if self.engine.board.turn == chess.BLACK]   # change for it to be general
+        return black_moves
+        
+
+    def play(self):
+        """
+        Make the move
+        """
+        legal_moves = self.get_moves()
+        move = random.choice(legal_moves)
+        self.engine.board.push(move)
+
