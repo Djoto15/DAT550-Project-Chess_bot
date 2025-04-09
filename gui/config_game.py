5c3da667dfc75db790a1920557be1609d710b455
Djoto15 <raph.gauthier02@gmail.com>
2025-04-09 17:52:56 +0200

    final commit: bot integration complete

--- gui/config_game.py
+++ gui/config_game.py
@@ -21,7 +21,7 @@ class ConfigGameDialog(QDialog):
         white_label = QLabel("White:")
         white_label.setAlignment(Qt.AlignCenter)
         self.white_player_combo = QComboBox()
-        self.white_player_combo.addItems(["Human", "RandomBot"])
+        self.white_player_combo.addItems(["Human", "Random bot"])
         white_layout.addWidget(white_label)
         white_layout.addWidget(self.white_player_combo)
 
